41af118b9a98da3888a11629fb3bf952cc82c38d
Thomas Dowrick <tdowrick@gmail.com>
2018-04-12 14:00:43 +0100

    Added Arduino code to same repo as schematics

---
+++ src/DDS_PCB/DDS_PCB.ino
@@ -0,0 +1,140 @@
+// Library for handling the SPI transfer
+#include <SPI.h>
+
+#include "definitions.h"
+#include "frequencies.h"
+#include <stdio.h>
+
+void setup() {
+
+  Serial.begin(SERIAL_BAUD_RATE);
+
+  pinMode(FSYNC1_Pin, OUTPUT);
+  pinMode(FSYNC2_Pin, OUTPUT);
+  pinMode(FSYNC3_Pin, OUTPUT);
+  pinMode(FSYNC4_Pin, OUTPUT);
+  pinMode(FSYNC5_Pin, OUTPUT);
+  pinMode(FSYNC6_Pin, OUTPUT);
+
+    //Set FSYNC pins High. FSYNC is active low
+  digitalWrite(FSYNC1_Pin, HIGH); 
+  digitalWrite(FSYNC2_Pin, HIGH); 
+  digitalWrite(FSYNC3_Pin, HIGH);
+  digitalWrite(FSYNC4_Pin, HIGH); 
+  digitalWrite(FSYNC5_Pin, HIGH); 
+  digitalWrite(FSYNC6_Pin, HIGH); 
+
+  // For SPI data transfer setup
+  pinMode(SDATA_SPI_Pin, OUTPUT);
+  pinMode(SCLK_SPI_Pin, OUTPUT);
+
+  digitalWrite(SDATA_SPI_Pin, LOW);
+  digitalWrite(SCLK_SPI_Pin, LOW);
+
+  // Arduino handles most of the underlying SPI transfer by itself, set it up here
+  SPI.begin();  // Enable SPI
+  SPI.setBitOrder(MSBFIRST);  // Send data Most significant bit first, this is necessary for the AD9833
+  SPI.setDataMode(SPI_MODE2);  // Set SPI Mode 2 (Data captured on falling edge of clock, clock inversion on)
+  SPI.setClockDivider(SPI_CLOCK_DIV128);    // Set clock divider (optional)
+}
+
+void loop() {
+
+  byte byteRead; // Serial read data
+  int val_to_program; // Frequency (Hz), phase (degrees), time etc.
+  int chan_to_program;
+  String what_to_program; //  Command to execute
+
+  // Allow setting of frequency/phase from serial monitor
+  // Input 'freq val_to_program' to program frequency
+  // 'phase val_to_program' to set phase
+
+  while (1) {
+    // Wait for Serial data to be received and parse it
+
+    delay(ONE_SECOND_DELAY);
+    Program_Freqs(Freqs, NUM_CHANNELS);
+    
+    if (Serial.available()) {
+
+      what_to_program = Serial.readStringUntil(' '); // Get a string
+      val_to_program = Serial.parseInt(); // Get an int
+      chan_to_program = Serial.parseInt();
+
+      // Process input and call appropriate commands
+      // Can't use switch statement to do this as it won't accept a string as a comparator
+      if (what_to_program == "reset") {
+        Reset_DDS(chan_to_program);
+      }
+
+      else if (what_to_program == "freq")     {
+        Set_AD9833_Frequency(val_to_program, chan_to_program);
+      }
+
+      else if (what_to_program == "phase")	 {
+        Set_AD9833_Phase(val_to_program, chan_to_program);
+      }
+
+      // default case - invalid command has been passed
+      else {
+        Serial.println("Command not recognised.");
+      }
+    }
+  }
+}
+
+
+// Additional functions that may be helpful
+
+void Program_Freqs (long Freqs [], int n_chans) {
+  
+  /* Loop through each freq/chan pair and program the switches/DDS chip */
+
+  Serial.println("Programming default frequencies");
+  for (int i = 0; i < n_chans; i++) {
+    Serial.print("Programming channel ");
+    Serial.println(i);
+    Set_AD9833_Frequency(Freqs[i], i + 1);
+  }
+}
+
+void Program_Then_Turn_Off(long freq, unsigned int on_time_milli, int chan) {
+
+  /*Program the DDS chip for a given time, then turn off */
+
+  //Program all of the channels, wait a specified time, then turn off.
+  Set_AD9833_Frequency(freq, chan);
+  delay(on_time_milli);
+  Reset_DDS(chan);
+
+}
+
+
+void Sweep_Freq (int freq_min, int freq_step, int freq_max, int on_time, int chan) {
+  /* Sweeps the frequency output on a channel, with increment and max value set by user.
+freq_min: starting frequency (Hz)
+freq_step: increment (Hz)
+freq_max: Stopping frequency (Hz)
+chan: which channel to program
+on_time: how long to program each channel for (milliseconds) */
+
+  for (int i = freq_min ; i <= freq_max; i = i + freq_step) {
+    Set_AD9833_Frequency(i, chan);
+    delay(on_time);
+  }
+}
+
+
+void Reset_DDS(int chan) {
+  /* Reset channel to midscale output (turns off AC output) */
+  AD9833_SendWord(RESET_CONTROL_REGISTER, chan);
+}
+
+
+void Reset_All() {
+  for (int i = 0; i < NUM_CHANNELS; i++) {
+    Reset_DDS(i);
+  }
+ 
+}
+
---
+++ src/DDS_PCB/definitions.h
@@ -0,0 +1,40 @@
+#ifndef definitions_h
+#define definitions_h
+
+// Arduino pin numberings
+#define SDATA_SPI_Pin 11  // SDATA_SPI - Data signal for SPI, used to program AD9833
+#define SCLK_SPI_Pin  13  // SCLK_SPI - Clock signal for SPI, used to program AD9833
+
+// Parallel CS FSYNC Pins for Pro Mini
+#define FSYNC1_Pin 2
+#define FSYNC2_Pin 3
+#define FSYNC3_Pin 4
+#define FSYNC4_Pin 15
+#define FSYNC5_Pin 16
+#define FSYNC6_Pin 17
+
+// Clock generator frequency, set by resistor R_SET on the PCB. This is used in the formula for setting the DDS sine wave frequency (Hz)
+#define DDS_CLOCK_FREQUENCY 10e6
+
+#define ONE_SECOND_DELAY 1000 //1000 milliseconds
+#define SERIAL_BAUD_RATE 9600
+
+#define PRINT_BUFFER_SIZE 50
+
+#define SWITCH_DELAY_TIME 2    // Time to delay after programming switches
+#define CONTROL_REGISTER_VALUE 0x2000    // Default value for control register of DDS chip
+#define PHASE_REGISTER_VALUE 0xC000      // Default value for phase register of DDS chip
+#define RESET_CONTROL_REGISTER 0x2100    // Reset control register of DDS to produce midscale output
+#define PHASE_REGISTER_MAX 4096 // Maximum value of phase register
+#define DDS_PROGRAM_CONSTANT 0x10000000 // 2^28
+#define MAX_FREQUENCY 1e5 //Maximum frequency value, used for sanity check.
+
+// Maximum value for injection when operating with limited length injections (milliseconds)
+#define MAX_CAUTIOUS_INJECTION 25000
+
+// Bit masks and shifts
+#define LSB_BIT_MASK 0x3fff // Gives the 14 LSB of an unsigned long
+#define SPI_BIT_MASK 0xFF // Gives 8 MSB of an unsigned int
+#define FREQ_REG_MASK 0x4000 	//Use Register 0 (set 2 MSB to 01). To use Register 1, set 2MSB to 10 (0x8000)
+
+#endif
